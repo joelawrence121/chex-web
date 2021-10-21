728f5590672d6fbf87ba0e5ffad8054166205a80
Joseph Lawrence <joelawrence121@hotmail.co.uk>
2021-10-21 16:24:00 +0100

    V2: Add routing in App component

--- chex-web/src/components/App.tsx
+++ chex-web/src/components/App.tsx
@@ -1,22 +1,37 @@
 import SingleMovePuzzle from "./SingleMovePuzzle";
 import React from "react";
 import NavbarScroller from "./NavbarScroller";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 const navigation = {
-    brand: { name: "Chexplanations", to: "/" },
+    brand: {name: "Chexplanations", to: "/"},
     links: [
-        { name: "Commentary", to: "/commentary" },
-        { name: "Single Move Puzzles", to: "/puzzles" }
+        {name: "Commentary", to: "/commentary"},
+        {name: "Single Move Puzzles", to: "/puzzles"}
     ]
 }
 
 const App: React.FC = () => {
-    const { brand, links } = navigation;
+    const {brand, links} = navigation;
     return (
-        <div className="App">
-            <NavbarScroller brand={brand} links={links}/>
-            <SingleMovePuzzle />
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/puzzles">
+                    <div className="App">
+                        <NavbarScroller brand={brand} links={links}/>
+                        <SingleMovePuzzle/>
+                    </div>
+                </Route>
+                <Route path="/commentary">
+                    <NavbarScroller brand={brand} links={links}/>
+                    <h1>Commentary</h1>
+                </Route>
+                <Route path="/">
+                    <NavbarScroller brand={brand} links={links}/>
+                    <h1>Home</h1>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
