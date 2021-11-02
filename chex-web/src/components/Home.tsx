d07c19632c4e01db2661037a61ed81487b5ee54b
Joe Lawrence <joelawrence121@hotmail.co.uk>
2021-11-02 16:13:31 +0000

    V4: Add type statistic to home page

--- chex-web/src/components/Home.tsx
+++ chex-web/src/components/Home.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../styles/Home.css';
+import BarChart from "./BarChart";
+import ChapiService from "../service/ChapiService";
+import Statistics from "../types/Statistics";
 
 const Home: React.FC = () => {
 
+    const [puzzleTypes, setPuzzleTypes] = useState<string[]>([])
+    const [puzzleCounts, setPuzzleCounts] = useState<number[]>([])
+
+    // retrieve statistics hook
+    useEffect(() => {
+        ChapiService.getStatistics()
+            .then(response => {
+                console.log(response)
+                setPuzzleTypes((response.data as unknown as Statistics).types)
+                setPuzzleCounts((response.data as unknown as Statistics).counts)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }, [])
+
+    function getYCount(type: string): number {
+        return puzzleCounts[puzzleTypes.indexOf(type)]
+    }
+
+    function getXLabel(type: string): string {
+        return type
+    }
+
     return (
         <section className="home-animated-grid">
-            <div className="card-stationary-a">
+            <div className="card-stationary a">
                 <h2>Welcome to Chexplanations!</h2>
                 COMP30030 & COMP30040
             </div>
-            <div className="card-stationary">
+            <div className="card-stationary b">
+                <BarChart values={puzzleTypes} yValueFn={getYCount} xLabelFn={getXLabel}/>
             </div>
-            <div className="card-stationary"></div>
-            <div className="card-stationary"></div>
+            <div className="card-stationary c"> </div>
         </section>
     );
 }
 
 export default Home;
+
---
+++ chex-web/src/types/Statistics.ts
@@ -0,0 +1,5 @@
+export default interface Statistics {
+    types: string[],
+    counts: number[],
+}
+
